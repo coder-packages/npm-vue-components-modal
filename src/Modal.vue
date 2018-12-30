5b3963797f305fba783cf084ac56ac9e21b51237
Alexander Manukian <alikmanukian@gmail.com>
2018-12-30 22:57:41 +0400

    initital commit

---
+++ src/Modal.vue
@@ -0,0 +1,17 @@
+<template>
+  <portal to="modals">
+    <div class="c-modal">
+      <div class="c-modal__backdrop"></div>
+      <div class="c-modal__scroller">
+        <div class="c-modal__wrapper">
+          <span class="c-modal__close-btn"></span>
+          <div class="c-modal__container">
+            <slot></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </portal>
+</template>
+
+<style lang="scss" src="./Modal.scss"></style>
